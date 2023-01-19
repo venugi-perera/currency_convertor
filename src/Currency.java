8158b4d662d4fdc922de4f60d67695217816090b
Peshala <peshala.20201256@iit.ac.lk>
2023-01-19 21:26:20 +0530

    Initial commit

---
+++ src/Currency.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Currency extends JFrame {
+    private JPanel panel1;
+    private JTextField txtfrom;
+    private JTextField txtamount;
+    private JButton convertButton;
+    private JComboBox txtto;
+
+    public Currency() {
+
+        convertButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Double tot;
+                Double amount = Double.parseDouble(txtamount.getText());
+
+                if (txtfrom.getText() == "USD" && txtto.getSelectedItem().toString() == "Srilankan Rupees") {
+                    tot = amount * 179.50;
+                    JOptionPane.showMessageDialog(panel1, "Your Amount will be " + tot.toString());
+                } else if (txtfrom.getText() == "USD" && txtto.getSelectedItem().toString() == "Indian Rupees") {
+                    tot = amount * 70.50;
+                    JOptionPane.showMessageDialog(panel1, "Your Amount will be " + tot.toString());
+                }
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+        Currency currency = new Currency();
+        currency.setContentPane(new Currency().panel1);
+        currency.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        currency.setVisible(true);
+        currency.pack();
+    }
+}
